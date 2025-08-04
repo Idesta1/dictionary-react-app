2af3d5a6218647cef0705c78500d0a504b4e44c8
Iglesia Desta <iglesiad25@gmail.com>
2025-08-04 11:41:57 +0200

    Update
    create new synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     console.log(props.meaning)
@@ -8,10 +9,11 @@ export default function Meaning(props) {
         {props.meaning.definitions.map(function(definition, index) {
            return (
             <div key={index}>
-            <p>{definition.definition}
+            <p><strong>Definition:</strong>{definition.definition}
                 <br/>
-              <em>{definition.example}</em>
+              <strong>Example:</strong>{definition.example}
         </p>
+        <Synonyms synonyms={definition.synonyms} />
                 </div>
            );
         })}
